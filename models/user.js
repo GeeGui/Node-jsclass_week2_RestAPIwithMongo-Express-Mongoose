7811b0d50607e989f211d058d9ec714ebde1c98a
GeeGui <142735326+GeeGui@users.noreply.github.com>
2024-01-20 01:12:55 -0800

    Passport Facebook

--- models/user.js
+++ models/user.js
@@ -14,8 +14,10 @@ const userSchema = new Schema({
     admin: {
         type: Boolean,
         default: false
-    }
-});
+    },    
+    facebookId: String
+    
+    });
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
